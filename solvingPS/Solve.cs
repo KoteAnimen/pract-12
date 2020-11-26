8daa9323a1f88ac472d5210889b8f5f37d2e0208
Kote\Kote_Animen <sverh.il@yandex.ru>
2020-11-26 20:32:53 +0300

    Добавлена библиотека с функциями решения периметра и площади квадрата

---
+++ solvingPS/Solve.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solvingPS
+{
+    public class Solve
+    {
+        public static int Perimeter(int sideA)
+        {
+            return sideA * 4;
+        }
+
+        public static int Area(int sideA)
+        {
+            return sideA * sideA;
+        }
+    }
+}
